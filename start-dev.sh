8ef4cc18a2444b2bba4161d47ec5e40abe98caf0
Z User <z@container>
2026-04-06 05:30:45 +0000

    fd3b3109-b073-4759-90dc-4994a18102a3

--- start-dev.sh
+++ start-dev.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 cd /home/z/my-project
-while true; do
+while :; do
   echo "[$(date '+%H:%M:%S')] Starting dev server..." >> /home/z/my-project/dev.log 2>&1
-  node node_modules/.bin/next dev -p 3000 >> /home/z/my-project/dev.log 2>&1
-  EXIT_CODE=$?
-  echo "[$(date '+%H:%M:%S')] Exited ($EXIT_CODE), restarting in 2s..." >> /home/z/my-project/dev.log 2>&1
-  sleep 2
+  bun run dev >> /home/z/my-project/dev.log 2>&1
+  EXIT=$?
+  echo "[$(date '+%H:%M:%S')] Exit code: $EXIT, restarting..." >> /home/z/my-project/dev.log 2>&1
+  sleep 1
 done
